61b77d2dc9a05c874ed95e14a686969915abd24c
dhavalpanchal <dhaval@topsinfosolutions.com>
2020-04-19 13:48:32 +0530

    delete

--- application/views/reset_password/include/footer.php
+++
@@ -1,4 +0,0 @@
-</div>
-<a style="display: none;" href="#top" id="back-to-top"><i class="fa fa-chevron-up"></i></a>
-</body>
-</html>
--- application/views/reset_password/include/template.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$this->load->view('reset_password/include/header');  
-$this->load->view($main_content);  
-$this->load->view('reset_password/include/footer');  
